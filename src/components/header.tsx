283a1c79b6c1ae36277cd6d9e755bcca96c247d2
mimo-to <rounakhati18@gmail.com>
2025-09-04 23:44:39 +0530

    03:Header-component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,8 @@ import { LogoContainer } from "./logo-container";
 import { NavigationRoutes } from "./navigation-routes";
 import { NavLink } from "react-router-dom";
 import { ProfileContainer } from "./profile-container";
-import { ToggleContainer } from "./toggle-container";
+import { ToggleContainer } from "./toogle-container";
+
 
 const Header = () => {
     const { userId } = useAuth();
@@ -15,7 +16,7 @@ const Header = () => {
             className={cn("w-full border-b duration-150 transition-all ease-in-out")}
         >
             <Container>
-                <div className="flex items-center gap-4 w-full">
+                <div className="flex items-center gap-4  w-full">
                     {/* logo section */}
                     <LogoContainer />
 
