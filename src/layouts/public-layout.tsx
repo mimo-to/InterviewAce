2531cfe1840c7b52b734ec2d49e2cb52424fcd4f
mimo-to <rounakhati18@gmail.com>
2025-09-05 20:15:13 +0530

    04:Firebase Configuration & User Database

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/header";
 import AuthHanlder from "@/handlers/auth-handler";
 import { Outlet } from "react-router-dom";
 
+
 export const PublicLayout = () => {
     return (
         <div className="w-full">
